b7dde589c64e4f1ea86c33530c93c66e53aec744
Sergio Pino <sergio@chappsolutions.com>
2018-04-03 23:07:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ from progress_bar import ProgressBar
 
 def main():
     progress_bar = ProgressBar("=")
-    progress_bar.load()
+    progress_bar.load(end=4)
     progress_bar = ProgressBar("/\\")
-    progress_bar.load()
-    progress_bar = ProgressBar(u"\u2588")
     progress_bar.load(end=5)
+    progress_bar = ProgressBar(u"\u2588")
+    progress_bar.load(end=3)
 
 
 if __name__ == '__main__':
